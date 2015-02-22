0dcd0a53ec9890120f6f59e03ea5728cd550b685
Baird Castleberry <baird@bairdcastleberry.com>
2015-02-22 15:04:59 -0600

    This should bring the frontend up to date, with autocomplete on address and the current css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,15 +14,4 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require addresscomplete
-
-
-// Instantiate a slider
-var BurnSlider = $("input.BurnSlider").slider();
-var OvergrownSlider = $("input.OvergrownSlider").slider();
-var ShinglesSlider = $("input.ShinglesSlider").slider();
-var TrashSlider = $("input.TrashSlider").slider();
-
-// Call a method on the slider
-var burnvalue = BurnSlider.getValue();
-
-console.log(burnvalue);
+//= require interface
